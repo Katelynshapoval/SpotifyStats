187ca1296bd75c221d2645aa3967b57bb8fb6d6e
Kateryna Shapoval <katelyn.shapoval@gmail.com>
2026-03-14 14:12:43 +0100

    basic redirecting

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from spotify_api import playlist_data
 
 app = Flask(__name__)
 
@@ -6,6 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route("/sendLink", methods=["POST"])
+def send_link():
+    playlist_url = request.form["playlist"]
+
+    data = playlist_data(playlist_url)
+
+    print(data)
+
+    return render_template("playlist.html", data=data)
 
 
 app.run(debug=False)
--- spotify_api.py
+++ spotify_api.py
@@ -1,7 +1,6 @@
 import requests
 import os
 from dotenv import load_dotenv
-from werkzeug.datastructures import Authorization
 
 load_dotenv()
 
@@ -9,13 +8,10 @@ TOKEN = os.getenv("SPOTIFY_ACCESS_TOKEN")
 
 
 def playlist_data(url):
-    playlist_id = "54ZA9LXFvvFujmOVWXpHga"
+    playlist_id = url.split("/")[-1].split("?")[0]
     endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}"
     headers = {
         "Authorization": f"Bearer {TOKEN}"
     }
     r = requests.get(endpoint, headers=headers)
     return r.json()
-
-
-print(playlist_data(""))
